0570fa1a44f37e0a692c7e1702e780ed57d5053a
soumyabratam6 <144446453+soumyabratam6@users.noreply.github.com>
2024-05-25 20:19:38 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,8 +1,9 @@
 //your JS code here. If required.
-const output = document.getElementById("output");
-const btn = document.getElementById("download-images-button");
-
-const images = [
+window.onload = function() {
+    const output = document.getElementById("output");
+    const btn = document.getElementById("download-images-button");
+    // rest of your code
+	const images = [
   { url: "https://picsum.photos/id/237/200/300" },
   { url: "https://picsum.photos/id/238/200/300" },
   { url: "https://picsum.photos/id/239/200/300" },
@@ -30,3 +31,7 @@ function downloadAndDisplayImages() {
     });
 }
 btn.addEventListener("click", downloadAndDisplayImages);
+}
+//const output = document.getElementById("output");
+//const btn = document.getElementById("download-images-button");
+
